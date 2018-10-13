68192b7d0745c7de4b4db6dcc971f7e9a3267482
Sergei Eremenko <finalitik@gmail.com>
2018-10-13 21:55:59 +0300

    Cleanup src directory

--- src/README.md
+++
@@ -1,5 +0,0 @@
-#Sources Papirus SMPlayer Themes
-depends for convert to png and build qrc to rcc:
-- libqt4-dev-bin
-- librsvg2-bin (rsvg-convert)
-- findutils
--- src/make-rcc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rcc -binary ePapirus.qrc -o ../ePapirus/ePapirus.rcc
-rcc -binary Papirus.qrc -o ../Papirus/Papirus.rcc
-rcc -binary PapirusDark.qrc -o ../PapirusDark/PapirusDark.rcc
-rcc -binary PapirusAdapta.qrc -o ../PapirusAdapta/PapirusAdapta.rcc
-rcc -binary PapirusAdaptaNokto.qrc -o ../PapirusAdaptaNokto/PapirusAdaptaNokto.rcc
-echo "done"
